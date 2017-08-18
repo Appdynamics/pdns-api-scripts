b0d6d879ef8bfbd818d7af4f9631e0bfa6bd423d
Mike Przybylski <mike.przybylski@appdynamics.com>
2017-08-18 16:30:53 -0700

    Completed testCreateARecordWithDefaults()

--- test/test_create_update_delete_a_record.sh
+++ test/test_create_update_delete_a_record.sh
@@ -291,7 +291,7 @@ testCreateAAndPTRRecordWithDefaults(){
     # create zone
     create-pdns-zone.sh $DEBUG_FLAG -C "$PDNS_CONF_DIR/pdns.conf" "$ZONE_NAME" "$PRIMARY_MASTER"
 
-    # attempt to create A record, with default record parameters
+    # attempt to create A record, -p flag, with default record parameters
     create_update-pdns-a-record.sh -p $DEBUG_FLAG -C "$PDNS_CONF_DIR/pdns.conf" $RECORD_NAME.$ZONE_NAME \
         $RECORD_IP
 
@@ -337,5 +337,61 @@ testCreateARecordWithDefaults(){
         DEBUG_FLAG=-d
     fi
 
-    fail "Test not fully implemented" #FIXME: placeholder
+    local DEBUG_FLAG
+    local ZONE_NAME=$(_random_alphanumeric_chars 3).$(_random_alphanumeric_chars 3).tld.
+    local PRIMARY_MASTER=primary.master.$ZONE_NAME
+    local RECORD_NAME=$(_random_alphanumeric_chars 11)
+    local RECORD_IP=$(_random_ipv4_octet).$(_random_ipv4_octet).$(_random_ipv4_octet).$(_random_ipv4_octet)
+    local TTL=86400
+    local SCRIPT_STDERR="$(mktemp)"
+
+    if [ "$ENABLE_DEBUG" == "true" ]; then
+        DEBUG_FLAG=-d
+    fi
+
+    # attempt to create A record
+    create_update-pdns-a-record.sh $DEBUG_FLAG -C "$PDNS_CONF_DIR/pdns.conf" $RECORD_NAME.$ZONE_NAME\
+        $RECORD_IP 2>"$SCRIPT_STDERR"
+
+    # assert that the creation attempt failed because the zone didn't exist
+    assertEquals  "Error: Zone '$ZONE_NAME' does not exist." "$(head -1 "$SCRIPT_STDERR")"
+    rm -f "$SCRIPT_STDERR"
+
+    # create zone
+    create-pdns-zone.sh $DEBUG_FLAG -C "$PDNS_CONF_DIR/pdns.conf" "$ZONE_NAME" "$PRIMARY_MASTER"
+
+    # attempt to create A record, with default record parameters
+    create_update-pdns-a-record.sh $DEBUG_FLAG -C "$PDNS_CONF_DIR/pdns.conf" $RECORD_NAME.$ZONE_NAME \
+        $RECORD_IP
+
+    _wait_for_cache_expiry
+
+    # assert that the A record was created with expected defaults
+    local DIG_TTL
+    local DIG_RECORD_IP
+    eval $($TEST_DIG $RECORD_NAME.$ZONE_NAME A | awk '
+        /[\t\s]A[\t\s]/{
+            if($1 == "'"$RECORD_NAME.$ZONE_NAME"'"){
+                print "DIG_TTL="$2;
+                print "DIG_RECORD_IP="$5
+            }
+        }
+    ')
+    assertEquals "A record ttl mismatch" "$TTL" "$DIG_TTL"
+    assertEquals "A record IP mismatch" "$RECORD_IP" "$DIG_RECORD_IP"
+
+    # assert that the A record's complementary PTR record was NOT created
+    local PTR_RECORD_NAME=$(get_ptr_host_part $RECORD_IP).$(get_ptr_zone_part $RECORD_IP)
+    local DIG_PTR_FQDN
+    eval $($TEST_DIG -x $RECORD_IP | awk '
+        /[\t\s]PTR[\t\s]/{
+            if($1 == "'"$PTR_RECORD_NAME"'"){
+                print "DIG_PTR_FQDN="$5;
+            }
+        }
+    ')
+    assertEquals "PTR record created when it shouldn't have been" "" "$DIG_PTR_FQDN"
+
+    # delete the zone
+    delete-pdns-zone.sh $DEBUG_FLAG -C "$PDNS_CONF_DIR/pdns.conf" "$ZONE_NAME"
 }
