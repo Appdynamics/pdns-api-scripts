b5cabb504163068f7bfd97de63236a8add4d38f5
Mike Przybylski <mike.przybylski@appdynamics.com>
2018-01-03 17:17:00 -0800

    saving state before running dh_make

--- scratch/notes.md
+++ scratch/notes.md
@@ -7,10 +7,37 @@ config files for API, etc.
   * `changelog` (maintained by `dch` see man page for more
   details)
   * `compat` debhelper compatibility number: `10` (and nothing else)
-  * `control` (package metadata, see `deb-control` man page for more
-  details)
-  * `copyright` empty for now.  Will need to figure out
-  AppD-legal-friendly content here.
+  * `control` (See
+  https://www.debian.org/doc/debian-policy/#document-ch-controlfields
+  for more details)
+  * `copyright` AppD-legal-friendly content here. (See
+  http://dep.debian.net/deps/dep5/ for more information)<BR>
+  Example:
+
+```
+Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: pdns-api-scripts
+Upstream-Contact: Mike Przybylski <mike.przybylski@appdynamics.com>
+Source: https://github.com/appdynamics/pdns-api-scripts
+
+Files: *
+Copyright: Copyright 2018, AppDynamics LLC and its affiliates
+License: Apache-2
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ .
+ http://www.apache.org/licenses/LICENSE-2.0
+ .
+ or view it in /usr/share/common-licenses/Apache-2.0
+ .
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+```
+
   * `rules` (debian-specific build-system rules)
   ```
   #!/usr/bin/make -f
@@ -18,4 +45,9 @@ config files for API, etc.
            dh $@ --buildsystem=cmake
    ```
   * `source` (directory)
-    * `format` (source tree layout version and type): `3.0 (git)`
+    * `format` (source tree layout version and type): `3.0 (git)` or
+    `3.0 (native)`
+  * `*package_name*.install` Useful for splitting a build into multiple
+  packages.  See `dh_install` man page for more details.
+## References
+*
--- share/pdns-api-scripts/test/test_create_update_delete_ptr_record.sh
+++ share/pdns-api-scripts/test/test_create_update_delete_ptr_record.sh
@@ -170,3 +170,209 @@ testCreateUpdatePtrRecordWithDefaults(){
     assertEquals "PTR TTL" "$TTL" "$DIG_TTL"
     assertEquals "PTR hostname" "$PTR_HOSTNAME" "$DIG_PTR_HOSTNAME"
 }
+#!/bin/bash
+
+# Copyright 2017, AppDynamics LLC and its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if ! [ "$DEBUG_SHELL" == true ]; then
+    DEBUG_SHELL=false
+fi
+
+declare PDNS_TEST_DATA_ROOT PDNS_PID PDNS_STDERR PDNS_STDOUT
+PDNS_TEST_DNS_PORT=5354
+PDNS_TEST_HTTP_PORT=8011
+CACHE_TTL=1
+SLEEP_TIME=$((CACHE_TTL+1))
+
+# Alias dig with recurring options
+# include +tcp so that dig fails fast if DNS server is down
+TEST_DIG="dig @localhost +noquestion +nocomments +nocmd +nostats +tcp -p $PDNS_TEST_DNS_PORT"
+
+_test_cleanup(){
+    if $DEBUG_SHELL; then
+        >&2 echo "Dropping to a shell for debugging purposes.  Exit to complete cleanup."
+        pushd $PDNS_TEST_DATA_ROOT
+        /bin/bash
+        popd
+    fi
+
+    if [ -n "$PDNS_PID" ]; then
+        if kill -TERM $PDNS_PID >/dev/null 2>&1; then
+            >&2 echo "Terminated pdns_server pid $PDNS_PID"
+        else
+            >&2 echo "pdns_server pid $PDNS_PID died prematurely.  STDERR below..."
+            >&2 cat "$PDNS_STDERR"
+        fi
+    fi
+    >&2 echo "Deleting $PDNS_TEST_DATA_ROOT"
+    rm -rf "$PDNS_TEST_DATA_ROOT"
+    # since we displace _shunit_cleanup() with 'trap _test_cleanup EXIT', call it after test-specific cleanup is
+    # complete
+    _shunit_cleanup EXIT
+}
+
+
+# $1: number of random alphanumeric characters to output
+_random_alphanumeric_chars(){
+    if [[ "$1" =~ ^[0-9]+$ ]]; then
+        cat /dev/urandom | LC_CTYPE=C tr -dc 'a-z0-9' | head -c $1
+    else
+        return 1
+    fi
+}
+
+# Echoes random number between 0 and 254
+_random_ipv4_octet(){
+    echo $((RANDOM % 255 ))
+}
+
+_wait_for_cache_expiry(){
+    local
+    if [ "$ENABLE_DEBUG" == "true" ]; then
+        >&2 echo "Waiting $SLEEP_TIME seconds for PDNS cache to expire"
+    fi
+    sleep $SLEEP_TIME
+}
+
+oneTimeSetUp(){
+    PDNS_TEST_DATA_ROOT="$(mktemp -d)"
+    PDNS_CONF_DIR="$PDNS_TEST_DATA_ROOT/conf"
+    PDNS_SQLITE_DIR="$PDNS_TEST_DATA_ROOT/var"
+    PDNS_STDOUT="$PDNS_TEST_DATA_ROOT/pdns.out"
+    PDNS_STDERR="$PDNS_TEST_DATA_ROOT/pdns.err"
+
+    trap _test_cleanup EXIT
+
+    # generate temporary pdns config / sqlite database
+    /init-pdns-sqlite3-db-and-config.sh -n -C "$PDNS_CONF_DIR" -D "$PDNS_SQLITE_DIR"\
+        -p $PDNS_TEST_DNS_PORT -P $CACHE_TTL -q $CACHE_TTL -H $PDNS_TEST_HTTP_PORT -s "$PDNS_TEST_DATA_ROOT"
+
+    # start pdns_server, redirect stdout, stderr to files in $PDNS_TEST_DATA_ROOT and background
+    >&2 echo "Starting test pdns_server from $PDNS_TEST_DATA_ROOT"
+    pdns_server --config-dir="$PDNS_CONF_DIR" > "$PDNS_STDOUT" 2> "$PDNS_STDERR" &
+    # save PID
+    PDNS_PID=$!
+
+    if ! ps -p $PDNS_PID; then
+        >&2 echo "pdns_server failed to start."
+        >&2 cat "$PDNS_STDERR"
+        exit 1
+    fi
+}
+
+ENABLE_DEBUG=true
+#!/bin/bash
+
+# Copyright 2017, AppDynamics LLC and its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if ! [ "$DEBUG_SHELL" == true ]; then
+    DEBUG_SHELL=false
+fi
+
+declare PDNS_TEST_DATA_ROOT PDNS_PID PDNS_STDERR PDNS_STDOUT
+PDNS_TEST_DNS_PORT=5354
+PDNS_TEST_HTTP_PORT=8011
+CACHE_TTL=1
+SLEEP_TIME=$((CACHE_TTL+1))
+
+# Alias dig with recurring options
+# include +tcp so that dig fails fast if DNS server is down
+TEST_DIG="dig @localhost +noquestion +nocomments +nocmd +nostats +tcp -p $PDNS_TEST_DNS_PORT"
+
+_test_cleanup(){
+    if $DEBUG_SHELL; then
+        >&2 echo "Dropping to a shell for debugging purposes.  Exit to complete cleanup."
+        pushd $PDNS_TEST_DATA_ROOT
+        /bin/bash
+        popd
+    fi
+
+    if [ -n "$PDNS_PID" ]; then
+        if kill -TERM $PDNS_PID >/dev/null 2>&1; then
+            >&2 echo "Terminated pdns_server pid $PDNS_PID"
+        else
+            >&2 echo "pdns_server pid $PDNS_PID died prematurely.  STDERR below..."
+            >&2 cat "$PDNS_STDERR"
+        fi
+    fi
+    >&2 echo "Deleting $PDNS_TEST_DATA_ROOT"
+    rm -rf "$PDNS_TEST_DATA_ROOT"
+    # since we displace _shunit_cleanup() with 'trap _test_cleanup EXIT', call it after test-specific cleanup is
+    # complete
+    _shunit_cleanup EXIT
+}
+
+
+# $1: number of random alphanumeric characters to output
+_random_alphanumeric_chars(){
+    if [[ "$1" =~ ^[0-9]+$ ]]; then
+        cat /dev/urandom | LC_CTYPE=C tr -dc 'a-z0-9' | head -c $1
+    else
+        return 1
+    fi
+}
+
+# Echoes random number between 0 and 254
+_random_ipv4_octet(){
+    echo $((RANDOM % 255 ))
+}
+
+_wait_for_cache_expiry(){
+    local
+    if [ "$ENABLE_DEBUG" == "true" ]; then
+        >&2 echo "Waiting $SLEEP_TIME seconds for PDNS cache to expire"
+    fi
+    sleep $SLEEP_TIME
+}
+
+oneTimeSetUp(){
+    PDNS_TEST_DATA_ROOT="$(mktemp -d)"
+    PDNS_CONF_DIR="$PDNS_TEST_DATA_ROOT/conf"
+    PDNS_SQLITE_DIR="$PDNS_TEST_DATA_ROOT/var"
+    PDNS_STDOUT="$PDNS_TEST_DATA_ROOT/pdns.out"
+    PDNS_STDERR="$PDNS_TEST_DATA_ROOT/pdns.err"
+
+    trap _test_cleanup EXIT
+
+    # generate temporary pdns config / sqlite database
+    /init-pdns-sqlite3-db-and-config.sh -n -C "$PDNS_CONF_DIR" -D "$PDNS_SQLITE_DIR"\
+        -p $PDNS_TEST_DNS_PORT -P $CACHE_TTL -q $CACHE_TTL -H $PDNS_TEST_HTTP_PORT -s "$PDNS_TEST_DATA_ROOT"
+
+    # start pdns_server, redirect stdout, stderr to files in $PDNS_TEST_DATA_ROOT and background
+    >&2 echo "Starting test pdns_server from $PDNS_TEST_DATA_ROOT"
+    pdns_server --config-dir="$PDNS_CONF_DIR" > "$PDNS_STDOUT" 2> "$PDNS_STDERR" &
+    # save PID
+    PDNS_PID=$!
+
+    if ! ps -p $PDNS_PID; then
+        >&2 echo "pdns_server failed to start."
+        >&2 cat "$PDNS_STDERR"
+        exit 1
+    fi
+}
+
+source /shunit2
